4e031da438af398dcf303663c3a6010a3add576d
Mathias Kahlen <mathias@kahlen.dk>
2015-10-12 16:33:17 +0200

    Database wrapper init

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,7 +1,26 @@
 package database;
 
-/**
- * Created by NH on 12-10-2015.
- */
+import java.sql.SQLException;
+
 public class Database {
+
+    DatabaseDriver db = new DatabaseDriver();
+
+    public void createUser()
+    {
+        try
+        {
+            db.getCreateUser().setString(1, "Michael");
+            db.getCreateUser().setString(2, "Jan");
+
+            db.getCreateUser().executeUpdate();
+        }
+        catch ( SQLException sqlException )
+        {
+            sqlException.printStackTrace();
+            db.close();
+        }
+    }
+
+    
 }
--- src/database/DatabaseDriver.java
+++ src/database/DatabaseDriver.java
@@ -1,7 +1,70 @@
 package database;
 
-/**
- * Created by NH on 12-10-2015.
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DatabaseDriver {
+
+    private static String sqlUrl = "jdbc:mysql://localhost:3306/mydb";
+    private static String sqlUser = "root";
+    private static String sqlPassword = "";
+
+    private Connection connection = null;
+
+    private PreparedStatement createUser = null;
+    private PreparedStatement selectAllUsers = null;
+
+
+    /**
+     * Bruges til at oprette forbindelse til databasen og indeholder preparedStatements.
+     */
+    public DatabaseDriver()
+    {
+        try
+        {
+            connection = DriverManager.getConnection(sqlUrl, sqlUser, sqlPassword);
+
+            createUser = connection.prepareStatement("INSERT INTO User(Firstname, Lastname) VALUES (?, ?)");
+
+            //General prepared statements
+            selectAllUsers = connection.prepareStatement("SELECT * FROM Users");
+
+
+
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Bruges til at lukke forbindelsen til databasen.
+     */
+public void close()
+{
+    try{
+        connection.close();
+    }
+    catch(SQLException e)
+    {
+        e.printStackTrace();
+    }
+}
+
+    public PreparedStatement getCreateUser() {
+        return createUser;
+    }
+
+    public static void main(String[] args) {
+
+
+        Database database = new Database();
+
+        database.createUser();
+
+    }
 }
