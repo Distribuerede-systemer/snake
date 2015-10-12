1b7d5b6c6d51532dc56452e996d53b32a4000ae6
Nicolai Østergaard <nicolai76@live.dk>
2015-10-12 19:33:28 +0200

    Created For-loop which autheticates user and password validation.

--- src/controller/Logic.java
+++ src/controller/Logic.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.ArrayList;
 import model.Game;
+import model.User;
 
 /**
  * This class contains all methods that interact between the TUI / API and the data-layer in the Model package of the application. 
@@ -18,14 +19,17 @@ public class Logic {
 	public ArrayList getUsers(){
 
 		// Define ArrayList to be used to add users and return them. 
-		ArrayList users = new ArrayList();	
+		ArrayList user = new ArrayList();
 		
 		// Return Users
-		return users;
+		return user;
+
+
 	}
+
 	
-	//Gets a list of all games and return these as a ArrayList of Game objects 
-	public ArrayList getGames(){
+	//Gets a list of all games and return these as an ArrayList of Game objects
+	public ArrayList getGames(String type){
 
 		// Define ArrayList to be used to add games and return them. 
 		ArrayList games = new ArrayList();	
@@ -35,7 +39,7 @@ public class Logic {
 	}
 
 	//Return an istance of a game
-	public Game getGame(String gameName) {
+	public Game createGame(String gameName) {
 
 		//int gameId, int result, String controls, int newGame, int endGame, String host, String opponent, String status
 		Game game = new Game(1, 1, "ASD", 1, 1, "localhost", "abcd", "HEJ");
@@ -51,7 +55,18 @@ public class Logic {
 	public int userLogin(String username, String password){
 		int code = 0;
 
-		//authenticate user with some function
+
+		for (User user : db.getUsers) {
+
+
+			if (user.getPassword.equals(password) && user.getUserName().equals(username)) {
+				System.out.println("Login Succeeded");
+			} else if (user.getPassword != (password) && user.getUserName().equals(username)) {
+				System.out.println("Wrong password");
+			} else {
+				System.out.print("User doesn't exist");
+			}
+		}
 
 		//Return status code for further use
 		return code;
@@ -69,5 +84,6 @@ public class Logic {
 		boolean status = false;
 
 		return status;
-	}	
+	}
+
 }
