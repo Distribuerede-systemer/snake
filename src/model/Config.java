6881277ea533aed036e6222c9097a820ddf88b08
Victor <vholving@gmail.com>
2015-10-12 14:37:37 +0200

    Update Config.java

--- src/model/Config.java
+++ src/model/Config.java
@@ -1,6 +1,5 @@
-// klassen anvendes som mellemled mellem serveren og config.json.dist
-// Json er udvekslingsformatet og Gson mapper disse til Java objekter
-// klassen skal kunne importeres fra DB-wrapper klassen
+// klassen anvendes som mellemled mellem serveren og config.json.dist som henter data fra klienterne
+// Serveren læser data direkte gennem klassen
 package model;
 /**
  * Created by Oscar on 12-10-2015.
