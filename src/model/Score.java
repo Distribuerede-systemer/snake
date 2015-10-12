e8c5cdeec84875f757b1b2b3fa6ce55e30cd97a1
Nicolai Østergaard <nicolai76@live.dk>
2015-10-12 13:56:09 +0200

    Created getters, setters and constructors.

--- src/model/Score.java
+++ src/model/Score.java
@@ -1,8 +1,33 @@
 package model;
 
+import com.google.gson.Gson;
+
 /**
  * Created by nicolaiostergaard on 12/10/15.
  */
+
+
+// Creates class Score
 public class Score {
-    
+
+    // Declare highScore
+    int highScore;
+
+    // Creates constructor
+    public Score(int _highScore)
+    {
+        highScore = _highScore;
+    }
+
+    // Creates get method which returns the highScore
+    public int getHighScore()
+    {
+        return highScore;
+    }
+
+    // Creates setter method
+    public void setHighScore(int _highScore)
+    {
+        highScore = _highScore;
+    }
 }
