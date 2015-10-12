2612d6facc56347e41f26c4017f3fee8cd164b1d
Nichlas <nh@jappes.dk>
2015-10-12 20:29:23 +0200

    Databasewrapper startup

--- src/database/DatabaseDriver.java
+++ src/database/DatabaseDriver.java
@@ -7,112 +7,14 @@ import java.sql.*;
 
 public class DatabaseDriver {
 
-    private static String sqlUrl = "jdbc:mysql://localhost:3306/testdb";
-    private static String sqlUser = "root";
-    private static String sqlPassword = "root";
-
-    private Connection connection = null;
-
-    private PreparedStatement createUser = null;
-    private ResultSet resultSet = null;
-
-    /**
-     * Bruges til at oprette forbindelse til databasen og indeholder preparedStatements.
-     */
-    public DatabaseDriver()
-    {
-        try
-        {
-            connection = DriverManager.getConnection(sqlUrl, sqlUser, sqlPassword);
-
-            createUser = connection.prepareStatement("INSERT INTO Users(Firstname, Lastname) VALUES (?, ?)");
-
-
-
-
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
-
-    /**
-     * Bruges til at lukke forbindelsen til databasen.
-     */
-    public void close()
-    {
-        try{
-            connection.close();
-        }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    public PreparedStatement getCreateUser() {
-        return createUser;
-    }
-
-    public CachedRowSetImpl getRecord(String table){
-
-        CachedRowSetImpl cachedResultSet = null;
-        try {
-            PreparedStatement getRecord = connection.prepareStatement("Select * from " + table);
-
-            resultSet = getRecord.executeQuery();
-            cachedResultSet = (CachedRowSetImpl) resultSet;
-
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-
-        return cachedResultSet;
-    }
-
-
-
-
-
-
-
-//    public User getUser(String userid)
-//    {
-//        User user = null;
-//        ResultSet resultSet = null;
+//    public String create() {
 //
-//        try{
-//            db.getSelectUserById().setString(1, userid);
-//            resultSet = db.getSelectUserById().executeQuery();
-//
-//            while(resultSet.next())
-//            {
-//                user = new User(
-//                        resultSet.getString("Name"),
-//                        resultSet.getString("UserID"),
-//                        resultSet.getString("Password"),
-//                        resultSet.getDouble("Balance"));
-//            }
-//        } catch (SQLException e)
-//        {
-//            e.printStackTrace();
-//        }
-//        finally{
-//            try{
-//                resultSet.close();
-//            }
-//            catch(SQLException ex)
-//            {
-//                ex.printStackTrace();
-//                db.close();
-//            }
-//        }
-//        return user;
 //    }
 
+    public String read(String table) {
 
+        String sqlStatement = "select * from " + table;
+
+        return sqlStatement;
+    }
 }
--- src/database/DatabaseWrapper.java
+++ src/database/DatabaseWrapper.java
@@ -1,12 +1,83 @@
 package database;
 
+import com.sun.rowset.CachedRowSetImpl;
+
+import java.sql.*;
+
 public class DatabaseWrapper {
 
-    DatabaseDriver db = new DatabaseDriver();
+    private static String sqlUrl = "jdbc:mysql://localhost:3306/testdb";
+    private static String sqlUser = "root";
+    private static String sqlPassword = "root";
 
-    public void getRecords(String table){
-        db.getRecord(table);
-        db.
+    private Connection connection = null;
+
+    private ResultSet resultSet = null;
+
+    DatabaseDriver dbDriver = new DatabaseDriver();
+
+    /**
+     * Bruges til at oprette forbindelse til databasen og indeholder preparedStatements.
+     */
+    public DatabaseWrapper()
+    {
+        try
+        {
+            connection = DriverManager.getConnection(sqlUrl, sqlUser, sqlPassword);
+
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Bruges til at lukke forbindelsen til databasen.
+     */
+    public void close()
+    {
+        try{
+            connection.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    public CachedRowSetImpl readDatabase(String table)
+    {
+        ResultSet resultSet = null;
+        CachedRowSetImpl cachedResult = null;
+        PreparedStatement ps;
+
+        try{
+            ps = connection.prepareStatement(dbDriver.read(table));
+            cachedResult = new CachedRowSetImpl();
+            resultSet = ps.executeQuery();
+//            while(resultSet.next()){
+//
+//            }
+
+            cachedResult.populate(resultSet);
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                resultSet.close();
+            }
+            catch(SQLException ex)
+            {
+                ex.printStackTrace();
+                close();
+            }
+        }
+        return cachedResult;
     }
 
     
