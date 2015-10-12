f7db178664581c859fcde5d601c33e90fbe80dbb
Victor <vholving@gmail.com>
2015-10-12 14:17:04 +0200

    Update Config.java

--- src/model/Config.java
+++ src/model/Config.java
@@ -1,5 +1,6 @@
-package model;
 // klasse modellerer data fra klienter og mapper JSON til java objekter
+// klassen skal kunne importeres fra DB-wrapper klassen
+package model;
 /**
  * Created by Oscar on 12-10-2015.
  */ 
