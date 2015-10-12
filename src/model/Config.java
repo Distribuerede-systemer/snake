73c5f0bddf1bb0e5e6993bacbd7599ee70a0755f
Oscar Brügger <obrugger@gmail.com>
2015-10-12 13:48:03 +0200

    Added Json innerclass to Config class. Created variables for each of the json-objects. Created getters.

--- src/model/Config.java
+++ src/model/Config.java
@@ -4,4 +4,37 @@ package model;
  * Created by Oscar on 12-10-2015.
  */
 public class Config {
+
+    public Config(){
+
+
+    }
+
+
+}
+
+class Json{
+
+    private String host;
+    private String port;
+    private String username;
+    private String password;
+    private String dbname;
+
+    public String getDbname() {
+        return dbname;
+    }
+    public String getHost() {
+        return host;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPort() {
+        return port;
+    }
+    public String getUsername() {
+        return username;
+    }
 }
