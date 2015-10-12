48d56d0cf480431729ff7b9ccced5807827b3802
Nichlas <nh@jappes.dk>
2015-10-12 14:41:37 +0200

    Hashing (No comments)

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,16 +1,23 @@
 package controller;
 
-
+/**
+ * @author Team Depardieu
+ * Usage: MainMethod for Security.java testing
+ * TODO: Delete after tests.
+ */
 public class Main {
 
     public static void main(String[] args) {
 
-        String depardieu = "whatever";
-        Security encrypt = new Security();
+        String depardieu = "{\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"}";
+        Security security = new Security();
+        String test = security.encrypt(depardieu, "j");
         System.out.println(depardieu);
-        System.out.println(encrypt.hashing(depardieu));
-
+        System.out.println(security.hashing(depardieu));
+        System.out.println(security.encrypt(depardieu, "j"));
+        System.out.println(security.decrypt(test, "j"));
+        if (depardieu.equals(security.decrypt(test, "j"))) {
+            System.out.println("True");
+        }
     }
-
-
 }
--- src/controller/Security.java
+++ src/controller/Security.java
@@ -2,11 +2,14 @@ package controller;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
+import sun.misc.BASE64Encoder;
+import sun.misc.BASE64Decoder;
 /**
  * This class contains methods which returns either a hashed or
  * encrypted/decrypted string from a string argument
  * @author Team Depardieu
+ * Usage: security.hashing(String), security.encrypt(String message, String key),
+ * security.decrypt(String message, String key)
  */
 public class Security {
 
@@ -45,5 +48,66 @@ public class Security {
         }
         return hexString.toString();
     }
-}
+
+    /**
+     * This method returns an encrypted (XOR) String
+     * Source: http://stackoverflow.com/questions/13641563/xor-cipher-in-java-php-different-results
+     * @param message
+     * @param key
+     * @return
+     */
+        public String encrypt(String message, String key){
+            try {
+                if (message==null || key==null ) return null;
+
+                char[] keys=key.toCharArray();
+                char[] mesg=message.toCharArray();
+                BASE64Encoder encoder = new BASE64Encoder();
+
+                int ml=mesg.length;
+                int kl=keys.length;
+                char[] newmsg=new char[ml];
+
+                for (int i=0; i<ml; i++){
+                    newmsg[i]=(char)(mesg[i]^keys[i%kl]);
+                }
+                mesg=null;
+                keys=null;
+                String temp = new String(newmsg);
+                return new String(new BASE64Encoder().encodeBuffer(temp.getBytes()));
+            }
+            catch ( Exception e ) {
+                return null;
+            }
+        }
+
+    /**
+     * This method returns a decrypted (XOR) String
+     * Source: http://stackoverflow.com/questions/13641563/xor-cipher-in-java-php-different-results
+     * @param message
+     * @param key
+     * @return
+     */
+        public String decrypt(String message, String key){
+            try {
+                if (message==null || key==null ) return null;
+                BASE64Decoder decoder = new BASE64Decoder();
+                char[] keys=key.toCharArray();
+                message = new String(decoder.decodeBuffer(message));
+                char[] mesg=message.toCharArray();
+
+                int ml=mesg.length;
+                int kl=keys.length;
+                char[] newmsg=new char[ml];
+
+                for (int i=0; i<ml; i++){
+                    newmsg[i]=(char)(mesg[i]^keys[i%kl]);
+                }
+                mesg=null; keys=null;
+                return new String(newmsg);
+            }
+            catch ( Exception e ) {
+                return null;
+            }
+        }}
 
