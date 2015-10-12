3bb066e16af813985cb11e665d77b25beec3f81c
Nichlas <nh@jappes.dk>
2015-10-12 18:10:33 +0200

    Databasewrapper startup

--- src/database/DatabaseDriver.java
+++ src/database/DatabaseDriver.java
@@ -1,21 +1,20 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import com.sun.rowset.CachedRowSetImpl;
+
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class DatabaseDriver {
 
-    private static String sqlUrl = "jdbc:mysql://localhost:3306/mydb";
+    private static String sqlUrl = "jdbc:mysql://localhost:3306/testdb";
     private static String sqlUser = "root";
-    private static String sqlPassword = "";
+    private static String sqlPassword = "root";
 
     private Connection connection = null;
 
     private PreparedStatement createUser = null;
-    private PreparedStatement selectAllUsers = null;
-
+    private ResultSet resultSet = null;
 
     /**
      * Bruges til at oprette forbindelse til databasen og indeholder preparedStatements.
@@ -26,10 +25,8 @@ public class DatabaseDriver {
         {
             connection = DriverManager.getConnection(sqlUrl, sqlUser, sqlPassword);
 
-            createUser = connection.prepareStatement("INSERT INTO User(Firstname, Lastname) VALUES (?, ?)");
+            createUser = connection.prepareStatement("INSERT INTO Users(Firstname, Lastname) VALUES (?, ?)");
 
-            //General prepared statements
-            selectAllUsers = connection.prepareStatement("SELECT * FROM Users");
 
 
 
@@ -44,27 +41,78 @@ public class DatabaseDriver {
     /**
      * Bruges til at lukke forbindelsen til databasen.
      */
-public void close()
-{
-    try{
-        connection.close();
-    }
-    catch(SQLException e)
+    public void close()
     {
-        e.printStackTrace();
+        try{
+            connection.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
-}
 
     public PreparedStatement getCreateUser() {
         return createUser;
     }
 
-    public static void main(String[] args) {
+    public CachedRowSetImpl getRecord(String table){
 
+        CachedRowSetImpl cachedResultSet = null;
+        try {
+            PreparedStatement getRecord = connection.prepareStatement("Select * from " + table);
 
-        Database database = new Database();
+            resultSet = getRecord.executeQuery();
+            cachedResultSet = (CachedRowSetImpl) resultSet;
 
-        database.createUser();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
 
+        return cachedResultSet;
     }
+
+
+
+
+
+
+
+//    public User getUser(String userid)
+//    {
+//        User user = null;
+//        ResultSet resultSet = null;
+//
+//        try{
+//            db.getSelectUserById().setString(1, userid);
+//            resultSet = db.getSelectUserById().executeQuery();
+//
+//            while(resultSet.next())
+//            {
+//                user = new User(
+//                        resultSet.getString("Name"),
+//                        resultSet.getString("UserID"),
+//                        resultSet.getString("Password"),
+//                        resultSet.getDouble("Balance"));
+//            }
+//        } catch (SQLException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        finally{
+//            try{
+//                resultSet.close();
+//            }
+//            catch(SQLException ex)
+//            {
+//                ex.printStackTrace();
+//                db.close();
+//            }
+//        }
+//        return user;
+//    }
+
+
 }
---
+++ src/database/DatabaseWrapper.java
@@ -0,0 +1,13 @@
+package database;
+
+public class DatabaseWrapper {
+
+    DatabaseDriver db = new DatabaseDriver();
+
+    public void getRecords(String table){
+        db.getRecord(table);
+        db.
+    }
+
+    
+}
