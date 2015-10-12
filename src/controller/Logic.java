73fe66bb5234f0ca7c8adfa62285388198e65054
Henrik Thorn <henrikthorn@it-kartellet.dk>
2015-10-12 15:55:00 +0200

    Added new Logic class for use between the API / TUI and the Model / data layer of the application

---
+++ src/controller/Logic.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * This class contains all methods that interact between the TUI / API and the data-layer in the Model package of the application. 
+ * @author Henrik Thorn
+ */
+public class Logic {
+
+	
+
+}
