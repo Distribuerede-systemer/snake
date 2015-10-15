9412ec8a9308a733c35d83c5911fa1ea9e424629
Jesper Bruun Hansen <jesper@mentalact.dk>
2015-10-15 15:36:48 +0200

    Created the GameEngine class to run the game itself. It will use the newly created Gamer, who is extending the user object.

--- src/controller/Logic.java
+++ src/controller/Logic.java
@@ -7,9 +7,11 @@ import java.sql.ResultSet;
 import java.util.*;
 
 
+import javafx.util.Pair;
 import model.Config;
 import model.Game;
 // imports information about password and username from the User class in the model package
+import model.Gamer;
 import model.User;
 import tui.Tui;
 
@@ -267,148 +269,13 @@ public class Logic {
 
 	//Deletes a game from the database
 	public boolean deleteGame(){
+
 		return false;
 	}
 
 	//Deletes a user from the database
 	//TODO: Create function.
 
-	public Array playGame(int size, String hostControls, String opponentControls){
-		// Dummy variables that should be replaced with data from the host:
-		String hostControls = "ddssaawwdddddd";
-		String opponentControls = "ddwwaass";
-		String turn = Math.round(Math.random() * 1) == 0 ? "host" : "opponent";
 
-		// Hosting party:
-		System.out.println("\n### GAME: host is " + turn + "! ###\n");
 
-		// Split each controls string into a character array:
-		char[] hostControlCharacters = hostControls.toCharArray();
-		char[] opponentControlCharacters = opponentControls.toCharArray();
-
-		// The game variables for the host:
-		boolean hostDidCrash = false;
-		int hostScore = 0;
-		int hostKills = 0;
-		int hostX = 0;
-		int hostY = 1;
-
-		// The game variables for the opponent:
-		boolean opponentDidCrash = false;
-		int opponentScore = 0;
-		int opponentX = 0;
-		int opponentY = -1;
-		int opponentKills = 0;
-
-		// Playing field options:
-		int boundary = 4;
-
-		// Total amount of moves:
-		int movesCount = hostControls.length() < opponentControls.length() ? opponentControls.length() : hostControls.length();
-
-		// Arrays of host and opponent moves:
-		int[][] hostMoves = new int[movesCount][];
-		int[][] opponentMoves = new int[movesCount][];
-
-		// Loop through the host (leader) moves:
-		System.out.println("Looping through host moves:\n");
-
-		for (int i = 0; i < hostControls.length(); i++) {
-			char move = hostControlCharacters[i];
-
-			if (move == 'a') {
-				hostX--;
-			} else if (move == 'd') {
-				hostX++;
-			} else if (move == 'w') {
-				hostY++;
-			} else if (move == 's') {
-				hostY--;
-			}
-
-			hostMoves[i] = new int[]{hostX, hostY};
-
-			System.out.println(Arrays.toString(hostMoves[i]));
-		}
-
-		// Loop through the follower (opponent) moves:
-		System.out.println("\nLooping through opponent moves:\n");
-
-		for (int i = 0; i < opponentControls.length(); i++) {
-			char move = opponentControlCharacters[i];
-
-			if (move == 'a') {
-				opponentX--;
-			} else if (move == 'd') {
-				opponentX++;
-			} else if (move == 'w') {
-				opponentY++;
-			} else if (move == 's') {
-				opponentY--;
-			}
-
-			opponentMoves[i] = new int[]{opponentX, opponentY};
-
-			System.out.println(Arrays.toString(opponentMoves[i]));
-		}
-
-		// Loop through both moves to find collisions:
-		System.out.println("\nDetecting collisions in favor of " + turn + ":\n");
-
-		for (int i = 0; i < movesCount; i++) {
-			int[] hostMove = hostMoves[i];
-			int[] opponentMove = opponentMoves[i];
-
-			// Kills:
-			if (hostMove != null && opponentMove != null && hostMove[0] == opponentMove[0] && hostMove[1] == opponentMove[1]) {
-				if (turn.equals("host")) {
-					hostKills++;
-
-					opponentDidCrash = true;
-				} else {
-					opponentKills++;
-
-					hostDidCrash = true;
-				}
-
-				System.out.println("Collision at [" + hostMove[0] + ", " + hostMove[1] + "]!\tHost " + hostKills + " - Opponent " + opponentKills);
-			}
-
-			// Host move:
-			if (hostMove != null) {
-
-				// Check if the host move results in a wall crash:
-				if (hostMove[0] > boundary || hostMove[1] > boundary) {
-					hostDidCrash = true;
-				}
-
-				// Check if the host move is not null and if the host did not previously crash:
-				if (!hostDidCrash) {
-					hostScore++;
-				}
-
-			}
-
-			// Opponent move:
-			if (opponentMove != null) {
-
-				// Check if the opponent move results in a wall crash:
-				if (opponentMove[0] > boundary || opponentMove[1] > boundary) {
-					opponentDidCrash = true;
-				}
-
-				// Check if the opponent move is not null and if the opponent did not previously crash:
-				if (!opponentDidCrash) {
-					opponentScore++;
-				}
-
-			}
-
-		}
-
-		System.out.println("hostScore: " + hostScore);
-		System.out.println("opponentScore: " + opponentScore);
-
-	}
-}
 }
