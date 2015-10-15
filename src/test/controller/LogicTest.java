af177b7869214e7d8588fb8d77c0f5eeb3c15f83
Emil Thomsen <emilaebt@gmail.com>
2015-10-15 11:06:38 +0200

    Test files added to project

---
+++ src/test/controller/LogicTest.java
@@ -0,0 +1,95 @@
+package test.controller; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Logic Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>okt 15, 2015</pre> 
+* @version 1.0 
+*/ 
+public class LogicTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUsers() 
+* 
+*/ 
+@Test
+public void testGetUsers() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getGames() 
+* 
+*/ 
+@Test
+public void testGetGames() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getGame(String gameName) 
+* 
+*/ 
+@Test
+public void testGetGame() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: userLogin(String username, String password) 
+* 
+*/ 
+@Test
+public void testUserLogin() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: deleteGame(int Id) 
+* 
+*/ 
+@Test
+public void testDeleteGame() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: deleteUser(int Id) 
+* 
+*/ 
+@Test
+public void testDeleteUser() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
