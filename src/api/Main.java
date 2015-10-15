883d82b7a2c20571f913fc59f93c4a61cfeb1ccb
tobiaslundjeremiassen <tobiasjeremiassen94@gmail.com>
2015-10-15 22:14:16 +0200

    Tilføjelse af DbExist() og checkConnection() metoder fra DataDriver til Main.

--- src/api/Main.java
+++ src/api/Main.java
@@ -20,6 +20,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Config.init();
+
         new DatabaseDriver.dbExist();
 
         new DatabaseDriver.checkConnection();
@@ -28,7 +30,6 @@ public class Main {
         tui.getuserMenu();
 
 
-        Config.init();
 
 
     }
