586adab5069e1cc4d11b40517e89c1dfe440d5f1
tobiaslundjeremiassen <tobiasjeremiassen94@gmail.com>
2015-10-15 18:29:36 +0200

    General code clean up

--- src/api/Main.java
+++ src/api/Main.java
@@ -2,9 +2,8 @@ package api;
 
 import java.io.IOException;
 import java.sql.*;
-import Config.Config;
-import logic.Tui;
-
+//import Config.Config;
+//import logic.Tui;
 
 /**
  * Created by Tobias on 15/10/15.
@@ -12,17 +11,14 @@ import logic.Tui;
 
 public class Main {
 
-    private static String sqlUrl = "jdbc:mysql://" + Config.getHost() + ":" + Config.getPort();
-    private static String sqlUser = Config.getUsername();
-    private static String sqlPassword = Config.getPassword();
-    private static String dbName = Config.getDbname();
-
+    private static String sqlUrl = "jdbc:mysql://localhost:10098"; //+ Config.getHost() + ":" + Config.getPort();
+    private static String sqlUser = "root";//Config.getUsername();
+    private static String sqlPassword = "";//Config.getPassword();
+    private static String dbName = "";//Config.getDbname();
     private static Connection connection = null;
-
 
     /**
      *Use a preparedstatment to run SQL on the database
-     *
      * @param sql
      */
 
@@ -34,13 +30,10 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
-
 
     /**
      * This methode check connection if there is a connection to the database
-     *
      * @param args
      * @throws IOException
      */
@@ -48,24 +41,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         try {
-
             connection = DriverManager.getConnection(sqlUrl, sqlUser, sqlPassword);
 
             if (connection.isValid(1000)) {
                 System.out.println("You are connected");
             } else {
-
                 System.out.println("Connection lost");
             }
-
         } catch (SQLException e) {
-
             e.printStackTrace();
             System.exit(1);
         }
     }
-
-
 
     /**
      * Checks if the databse exists or not
@@ -77,31 +64,25 @@ public class Main {
     protected static boolean DbExist() throws SQLException {
 
         ResultSet resultSet = connection.getMetaData().getCatalogs();
-
         while (resultSet.next()) {
-
             String databaseName = resultSet.getString(1);
 
             if (databaseName.equals(dbName)) {
-
                 return true;
 
             } else {
-
                 doQuery("Indsæt SQL dump");
-
             }
         }
 
         return false;
     }
 
-
     /**
      * Create a object of tui class.
      * This method starts the program
      */
 
-    new tui();
+    //new tui();
 }
 
